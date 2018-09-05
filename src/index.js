fa04232b34bf8500aae82be7ff8c91e3f78877d7
Your Name <you@example.com>
2018-09-05 20:58:58 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstra‌​p.min.css '
+// import '/home/nelson/daruso-coict/node_modules/bootstrap/dist/css/bootstrap.css '
 import App from './App';
 import Click from './components/Click';
 
