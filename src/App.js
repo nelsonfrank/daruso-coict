f162365c9cdac07e36f7d988561fa12a45a56e0f
Your Name <you@example.com>
2018-09-05 23:16:34 +0300

    changes made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router,  Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,  Route,} from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/header/Navbar';
