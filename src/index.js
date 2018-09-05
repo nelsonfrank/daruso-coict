b6a30348f4d09719916584d94475b65fc9e25f5b
Your Name <you@example.com>
2018-09-05 20:49:45 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstra‌​p.min.css '
 import App from './App';
 import Click from './components/Click';
 
